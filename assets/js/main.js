19ae2a36ecd47265bcd30416406b5322437d8da0
Nikita Koshik <nikitakoshik064@gmail.com>
2022-05-28 13:19:05 +0300

    28.05.2022

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,56 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     const form = document.getElementById('form');
-//     form.addEventListener('submit', formSend);
-
-//     async function formSend(e) {
-//         e.preventDefault();
-
-//         let error = formValidate(form);
-//     }
-
-
-//     function formValidate(form) {
-//         let error = 0;
-//         let formReq = document.querySelectorAll('._req');
-
-//         for (let index = 0; index < formReq.length; index++) {
-//             const input = formReq[index];
-//             formRemoveError(input);
-
-//             if (input.classList.contains('_email')) {
-//                 if (emailTest(input)) {
-//                     formAddError(input);
-//                     error++;
-//                 }
-//             } else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
-//                 formAddError(input);
-//                 error++;
-//             } else {
-//                 if (input.value === '') {
-//                     formAddError(input);
-//                     error++;
-//                 }
-//             }
-//         }
-//     }
-
-//     function formAddError(input) {
-//         input.parentElement.classList.add('_error');
-//         input.classList.add('_error');
-//     }
-
-//     function formRemoveError(input) {
-//         input.parentElement.classList.remove('_error');
-//         input.classList.remove('_error');
-//     }
-
-//     function emailTest(input) {
-//         return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-//     }
-// });
-
-
-
 $(function() {
 
     const swiper = new Swiper('.swiper-container', {
@@ -169,23 +116,6 @@ $(function() {
         event.preventDefault();
         nav.toggleClass("show");
     });
-
-
-
-    // $(document).ready(function() {
-    //     $('#gradient').click(function() {
-    //         $('body').css('background', 'linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)');
-    //         $('body').css('background-size', '400% 400%');
-    //         $('body').css('animation', 'gradient 10s ease infinite');
-    //         $('.work').css('box-shadow', 'none');
-
-    //     })
-    //     $('#white').click(function() {
-    //         $('.work').css('box-shadow', '2px 2px 15px #000');
-    //         $('body').css('background', '#fff');
-
-    //     })
-    // });
 
     const switchEl = document.getElementById('backgroundSwitcher');
 
